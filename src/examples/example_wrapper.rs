cacd494a52ef6a2b423467101cdc4a5440e97f5e
Robert Bermani <rbermani@users.noreply.github.com>
2021-12-20 19:48:19 -0500

    cargo fix

--- src/examples/example_wrapper.rs
+++ src/examples/example_wrapper.rs
@@ -44,7 +44,7 @@ impl ExampleWrapper {
 
     pub fn process_event(&mut self) -> Result<(), IBKRApiLibError> {
         match self.client.get_event()? {
-            Some(ServerRspMsg::NextValidId  { version, order_id }) =>
+            Some(ServerRspMsg::NextValidId  { version: _, order_id }) =>
             {
                 self.next_order_id = order_id;
                 info!("next_valid_id -- order_id: {}", order_id);
@@ -52,19 +52,19 @@ impl ExampleWrapper {
                     panic!("start_requests failed!");
                 }
             },
-            Some(ServerRspMsg::ErrMsg { version, req_id, error_code, error_str }) => self.error(req_id, error_code, &error_str),
-            Some(ServerRspMsg::TickPrice { version, req_id, tick_type, price, tick_attr }) =>
+            Some(ServerRspMsg::ErrMsg { version: _, req_id, error_code, error_str }) => self.error(req_id, error_code, &error_str),
+            Some(ServerRspMsg::TickPrice { version: _, req_id, tick_type, price, tick_attr }) =>
                 info!("tick_size -- req_id: {}, tick_type: {}, price: {}, attrib: {}", req_id, tick_type, price, tick_attr),
-            Some(ServerRspMsg::TickSize { version, req_id, tick_type, size }) =>
+            Some(ServerRspMsg::TickSize { version: _, req_id, tick_type, size }) =>
                 info!( "tick_size -- req_id: {}, tick_type: {}, size: {}", req_id, tick_type, size),
             Some(ServerRspMsg::CompletedOrder { contract, order, order_state }) =>
                 info!("completed_order -- contract: [{}], order: [{}], order_state: [{}]", contract, order, order_state),
             Some(ServerRspMsg::CompletedOrdersEnd)  => info!("completed_orders_end -- (no parameters for this message)"),
             Some(ServerRspMsg::OrderBound {req_id, api_client_id, api_order_id} ) =>
                 info!( "order_bound -- req_id: {}, api_client_id: {}, api_order_id: {}", req_id, api_client_id, api_order_id),
-            Some(ServerRspMsg::MarketDataType { version, req_id, market_data_type}) =>
+            Some(ServerRspMsg::MarketDataType { version: _, req_id, market_data_type}) =>
                 info!("market_data_type -- req_id: {}, market_data_type: {}", req_id, market_data_type),
-            Some(ServerRspMsg::ManagedAccts { version, accounts_list }) =>
+            Some(ServerRspMsg::ManagedAccts { version: _, accounts_list }) =>
                 info!("managed_accounts -- accounts_list: {}", accounts_list),
             Some(ServerRspMsg::OpenOrderEnd) => info!("open_order_end. (no parameters passed)"),
             Some(ServerRspMsg::OpenOrder { order_id, contract, order, order_state }) =>
@@ -78,8 +78,8 @@ impl ExampleWrapper {
                     client_id, why_held, mkt_cap_price),
             Some(ServerRspMsg::ExecutionData { req_id, contract, execution }) =>
                 info!("exec_details -- req_id: {}, contract: {}, execution: {}", req_id, contract, execution),
-            Some(ServerRspMsg::ExecutionDataEnd { version, req_id }) => info!("exec_details_end -- req_id: {}", req_id),
-            Some(ServerRspMsg::CommissionReport { version, commission_report }) =>
+            Some(ServerRspMsg::ExecutionDataEnd { version: _, req_id }) => info!("exec_details_end -- req_id: {}", req_id),
+            Some(ServerRspMsg::CommissionReport { version: _, commission_report }) =>
                 info!("commission_report -- commission_report: {}", commission_report),
             Some(i) => panic!("Received unhandled event! Exiting. Event: {}", i.to_string()),
             None => (),
